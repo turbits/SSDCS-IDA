3da99c12f19c0815bda05ad4afc4c5cd855f39df
turbits <turbits@protonmail.com>
2023-02-22 19:20:09 -0700

    fix indentation to 4 spaces

--- config/settings.py
+++ config/settings.py
@@ -59,7 +59,7 @@ TEMPLATES = [
     },
 ]
 
-#WSGI_APPLICATION = 'wsgi.application'
+# WSGI_APPLICATION = 'wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
