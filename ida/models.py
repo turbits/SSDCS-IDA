00e0df40ccbb8359f6f2871232589b6c8d56d304
turbits <turbits@protonmail.com>
2023-02-26 18:06:08 -0700

    add file model

--- ida/models.py
+++ ida/models.py
@@ -1,3 +1,17 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+# User model is included by default in Django from the django.contrib.auth system
+
+
+class File(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    extension = models.CharField(max_length=255)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+    lastUser = models.ForeignKey(models.User, on_delete=models.SET('Deleted User'), null=True, blank=True)
+    category = models.CharField(max_length=100, null=True, blank=True)
+    filesize = models.IntegerField()
+    file = models.FileField(upload_to='files/')
