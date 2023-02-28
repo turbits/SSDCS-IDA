ce32a0a0e7970bf4e23bb6c21a72b8d10a820592
turbits <turbits@protonmail.com>
2023-02-28 12:55:29 -0700

    add index page, styles, fix static dir

--- ida/views.py
+++ ida/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("IDA Project")
+    # data can be put here that the page might need
+    
+    # if there were data to pass, you would write this like this
+    # context = {
+    #     'test_context': 'testvalue'
+    # }
+    # return render(request, 'index.html', context=context)
+    # then the page would have access to
+    # the value of: context.test_context
+    # more here: https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Home_page
+    return render(request, 'index.html')
+
+
+def dataview(request):
+    return render(request, 'dataview.html')
