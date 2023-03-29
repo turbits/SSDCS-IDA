ea0c32a9abf2f70a83757186f5f518701c6697c0
turbits <turbits@protonmail.com>
2023-03-28 22:30:15 -0700

    refactor database generation and data types, add data(class model)/username/name/pw validation functions

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 import sqlite3
 import os
 import uuid
+import time
 
 # TODO: add a warning/confirmation if the database file already exists
 
@@ -19,7 +20,7 @@ cursor = connection.cursor()
 # This is a table that stores a UUID and a user id (foreign key)
 #
 # Q: What is this used for?
-# A: This is to avoid not using a more complex session system (for simplicity) and to allow for a more secure way of authenticating users other than just storing the user in a cookie (plain text)
+# A: This is to avoid not using a more complex session system (for simplicity) and to allow for a more secure way of authenticating users other than just storing all of the user data in a cookie (plain text)
 # When a user is created, a UUID is generated and stored in this table alongside the user ID
 # When an API request is made, the UUID is sent in the request header (the cookie) and the user is queried to check if they are flagged as an admin on the backend.
 # In a real-world scenario, a sessions library or other framework should probably be used
@@ -32,35 +33,40 @@ cursor.execute("CREATE TABLE IF NOT EXISTS user_ref (id INTEGER PRIMARY KEY AUTO
 
 # ===================== USERS TABLE =====================
 # CREATE USERS TABLE
-cursor.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, first_name TEXT, last_name TEXT, password TEXT, last_logon DATETIME, created_at DATETIME, is_admin BOOLEAN, is_disabled BOOLEAN)")
+cursor.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, first_name TEXT, last_name TEXT, password TEXT, last_logon INTEGER, created_at INTEGER, is_admin BOOLEAN, is_disabled BOOLEAN)")
 
 # CREAT DEFAULT ADMIN
-cursor.execute("INSERT INTO users (username, first_name, last_name, password, last_logon, created_at, is_admin, is_disabled) VALUES ('admin', 'Default', 'Admin', 'admin123', DATE('now'), DATE('now'), 1, 0)")
+cursor.execute("INSERT INTO users (username, first_name, last_name, password, last_logon, created_at, is_admin, is_disabled) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", ('admin', 'Default', 'Admin', 'admin123', None, int(time.time()), True, False,))
 # get admin id
-cursor.execute("SELECT id FROM users WHERE username = 'admin'")
-admin_id = cursor.fetchone()[0]
+cursor.execute("SELECT * FROM users WHERE username = 'admin'")
+admin = cursor.fetchone()
+admin_id = admin[0]
+admin_username = admin[1]
 # add user_ref entry
-cursor.execute(f"INSERT INTO user_ref (uuid, user_id) VALUES ('{str(uuid.uuid4())}', {admin_id})")
+cursor.execute("INSERT INTO user_ref (uuid, user_id) VALUES (?, ?)", (str(uuid.uuid4()), admin_id,))
 
 # CREATE TESTUSER
-cursor.execute("INSERT INTO users (username, first_name, last_name, password, last_logon, created_at, is_admin, is_disabled) VALUES ('testuser', 'Test', 'User', 'testuser123', DATE('now'), DATE('now'), 0, 0)")
+cursor.execute("INSERT INTO users (username, first_name, last_name, password, last_logon, created_at, is_admin, is_disabled) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", ('testuser', 'Test', 'User', 'testuser123', None, int(time.time()), False, False,))
 # get testuser id
-user_id = cursor.execute("SELECT id FROM users WHERE username = 'testuser'")
-user_id = cursor.fetchone()[0]
+cursor.execute("SELECT * FROM users WHERE username = 'testuser'")
+user = cursor.fetchone()
+user_id = user[0]
+user_username = user[1]
 # add user_ref entry
-cursor.execute(f"INSERT INTO user_ref (uuid, user_id) VALUES ('{str(uuid.uuid4())}', {user_id})")
+cursor.execute("INSERT INTO user_ref (uuid, user_id) VALUES (?, ?)", (str(uuid.uuid4()), user_id,))
 
 # ===================== RECORDS TABLE =====================
 # CREATE RECORDS TABLE
 cursor.execute("CREATE TABLE IF NOT EXISTS records (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, created_at DATETIME, revised_at DATETIME, file TEXT)")
 # seed with some data
-cursor.execute("INSERT INTO records (name, created_at, revised_at, file) VALUES ('Record 1', DATE('now'), DATE('now'), '{''data'': ''Record 1''}')")
+cursor.execute("INSERT INTO records (name, created_at, revised_at, file) VALUES (?, ?, ?, ?)", ('Record 1', int(time.time()), None, "{'data': 'Record 1'}",))
 
 # ===================== LOGS TABLE =====================
 # CREATE LOGS TABLE
 cursor.execute("CREATE TABLE IF NOT EXISTS logs (id INTEGER PRIMARY KEY AUTOINCREMENT, status TEXT, message TEXT, created_at DATETIME, author_id INTEGER, author_name TEXT)")
 # add initial LogEvent
-cursor.execute("INSERT INTO logs (status, message, created_at, author_id, author_name) VALUES ('INFO', 'Initial LogEvent', DATE('now'), 1, 'admin')")
+cursor.execute("INSERT INTO logs (status, message, created_at, author_id, author_name) VALUES (?, ?, ?, ?, ?)", ('INFO', 'Initial LogEvent', int(time.time()), admin_id, admin_username,))
 
-# CLOSE CONNECTION
+# COMMIT & CLOSE CONNECTION
+connection.commit()
 connection.close()
--- ida.py
+++ ida.py
@@ -1,8 +1,8 @@
 import os
 import sys
-from bottle import run, debug, static_file, abort, error, route
+from bottle import Bottle, run, debug, static_file, abort, error, route
 
-sys.path.insert(0, os.path.dirname("routes/"))
+# sys.path.insert(0, os.path.dirname("routes/"))
 
 # any "os.getenv" is getting values from the .env file
 # .env should be in the project root (same level as this file)
@@ -22,15 +22,13 @@ def send_static(filename):
 # making this file and the routes files easier to read and understand
 # noqa is used here to ignore the flake8 errors from a linting perspective
 # if this were a real product, you should not do this
-from routes import pages, logs, records, users  # noqa: E402, F401
+from routes import default, logs, records, users  # noqa: E402, F401
 
 
 @error(404)
-def error404(error):
+def error404(e):
     abort("404")
 
 
-# get debug env value, default to False
-debug(os.getenv("DEBUG", False))
-# run the app on localhost:8080, and reload if RELOADER env value is True; default to False
-run(host='localhost', port=8080, reloader=os.getenv("RELOADER", False))
+# run the app on localhost:8080; reloader will restart the server when a file is changed
+run(host='localhost', port=8080, reloader=True)
