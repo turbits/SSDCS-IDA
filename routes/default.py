c38a058e687dfe90b7399f1738186a068806de8b
turbits <turbits@protonmail.com>
2023-03-30 06:22:49 -0700

    refactor

--- routes/default.py
+++ routes/default.py
@@ -1,42 +1,42 @@
-from bottle import template, get, post, request, response
+from bottle import template, route, request, response
 from utility.validate_data import validate_username, validate_password
 
 
 # GETs
-@get('/')
+@route('/', method='GET')
 def render_index():
     return template('templates/index.tpl')
 
 
-@get('/login')
+@route('/login', method='GET')
 def render_login():
     return template('templates/login/index.tpl')
 
 
-@get('/dashboard')
+@route('/dashboard', method='GET')
 def render_dashboard():
     return template('templates/dashboard/index.tpl')
 
 
 # POSTs
-@post('/login')
+@route('/login', method='POST')
 def login():
     # get the username and password from the form
     username = request.forms.get('username')
     password = request.forms.get('password')
-    
+
     # validate username and pw
     _username_check = validate_username(username)
     _password_check = validate_password(password)
-    
+
     # if either of the checks fail, return an error
     if _username_check is not True or _password_check is not True:
         return template('templates/login/index.tpl', error='Invalid username or password.')
-    
+
     cookie_val = str('the UUID of the user')
     # if we were to harden this, we could add the 'domain' parameter, among others: https://www.freecodecamp.org/news/web-security-hardening-http-cookies-be8d8d8016e1/ (Nadalin, 2018)
     response.set_cookie('user_uuid', cookie_val, path='/', max_age=3600)
-    
+
     return template('templates/dashboard/index.tpl')
 
 # Nadalin, A. (2018) Web Security: How to Harden your HTTP cookies. Available at: [https://www.freecodecamp.org/news/web-security-hardening-http-cookies-be8d8d8016e1/](https://www.freecodecamp.org/news/web-security-hardening-http-cookies-be8d8d8016e1/) [Accessed 28 March 2023]
