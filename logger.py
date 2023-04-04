bfe5881a24c8717e28e49d1ee20dec17f0ff125a
turbits <turbits@protonmail.com>
2023-04-04 10:47:23 -0700

    start on logger system

--- logger.py
+++ logger.py
@@ -1,5 +1,10 @@
 import uuid
 import datetime
+from models import LogEvent
+from utility.enums import LogLevel
+
+
+# Logger should log events both to the server console and to the database
 
 
 # logger.create(logevent)
